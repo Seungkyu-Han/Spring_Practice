efee59f7240994186df6d7a05e1633fd453f84ab
Seungkyu-Han <98071131+Seungkyu-Han@users.noreply.github.com>
2023-02-06 20:49:45 +0900

    Add files via upload

---
+++ day6/core/src/main/java/hello/core/CustomerApp.java
@@ -0,0 +1,26 @@
+package hello.core;
+
+import hello.core.customer.Customer;
+import hello.core.customer.CustomerService;
+import hello.core.customer.CustomerServiceImpl;
+import hello.core.customer.Grade;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CustomerApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        CustomerService customerService = applicationContext.getBean("customerService", CustomerService.class);
+
+        Customer customer = new Customer(1L, "Seungkyu", Grade.VIP);
+
+        customerService.join(customer);
+
+        Customer findCustomer = customerService.findCustomer(1L);
+        System.out.println("new customer = " + customer.getName());
+        System.out.println("find customer = " + findCustomer.getName());
+    }
+}
